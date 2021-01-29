ff6e6482f5a8adc44a386408c28a206b091be32f
cohunachick <78184004+cohunachick@users.noreply.github.com>
2021-01-29 12:25:31 +1100

    upload

--- rio island/Assets/Plugins/upload.bat
+++ rio island/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\becky-vm\Documents\unity-stuff\rio island\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1651224239098822679.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@I:\users\docs\GitHub\rio_island\rio island\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1660577902770520255.json
